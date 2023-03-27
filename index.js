97b503a35a3c90ce64a05c457fef89dca5a17043
COLTON <74864526+COLTON-28@users.noreply.github.com>
2023-03-27 11:30:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+let request = require('request');
+
+let apiKey = '4ec3415d52d48eda1a129820cb14eb95';
+let city = 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
